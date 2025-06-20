417e02bc50cf90517dcea2196e2cde1cbf461438
GStech-code <gal.sabo96@campus.technion.ac.il>
2025-06-20 22:39:46 +0300

    test

--- Air Hockey/Scripts/Main.gd
+++
@@ -1,6 +0,0 @@
-# Main.gd
-extends Node
-
-func _ready():
-	var main_menu_scene := preload("res://Scenes/MainMenu.tscn").instantiate()
-	add_child(main_menu_scene)
--- Air Hockey/Scripts/puck.gd
+++
@@ -1,3 +0,0 @@
-class_name Puck
-
-extends RigidBody2D
